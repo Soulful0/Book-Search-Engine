56fbe9ca949a83fe40e38357c776b64926bbd08a
Soul <joshgingold336@gmail.com>
2024-07-12 12:39:31 -0700

    recreated api.js to handle apollo client for graphql operation

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -1,53 +1,148 @@
-// route to get logged in user's info (needs the token)
-export const getMe = (token) => {
-  return fetch('/api/users/me', {
-    headers: {
-      'Content-Type': 'application/json',
-      authorization: `Bearer ${token}`,
-    },
-  });
+import { gql } from "@apollo/client";
+import client from "../App";
+
+// Define your queries and mutations
+const GET_ME = gql`
+  query me {
+    me {
+      _id
+      username
+      email
+      savedBooks {
+        bookId
+        authors
+        description
+        title
+        image
+        link
+      }
+    }
+  }
+`;
+
+const LOGIN_USER = gql`
+  mutation login($email: String!, $password: String!) {
+    login(email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+      }
+    }
+  }
+`;
+
+const ADD_USER = gql`
+  mutation addUser($username: String!, $email: String!, $password: String!) {
+    addUser(username: $username, email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+      }
+    }
+  }
+`;
+
+const SAVE_BOOK = gql`
+  mutation saveBook($bookData: BookInput!) {
+    saveBook(bookData: $bookData) {
+      _id
+      username
+      email
+      savedBooks {
+        bookId
+        authors
+        description
+        title
+        image
+        link
+      }
+    }
+  }
+`;
+
+const REMOVE_BOOK = gql`
+  mutation removeBook($bookId: String!) {
+    removeBook(bookId: $bookId) {
+      _id
+      username
+      email
+      savedBooks {
+        bookId
+        authors
+        description
+        title
+        image
+        link
+      }
+    }
+  }
+`;
+
+// Helper functions to execute the GraphQL operations
+export const getMe = async () => {
+  try {
+    const { data } = await client.query({
+      query: GET_ME,
+    });
+    return data;
+  } catch (err) {
+    console.error(err);
+    throw new Error("Something went wrong");
+  }
 };
 
-export const createUser = (userData) => {
-  return fetch('/api/users', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify(userData),
-  });
+export const createUser = async (userData) => {
+  try {
+    const { data } = await client.mutate({
+      mutation: ADD_USER,
+      variables: userData,
+    });
+    return data;
+  } catch (err) {
+    console.error(err);
+    throw new Error("Something went wrong");
+  }
 };
 
-export const loginUser = (userData) => {
-  return fetch('/api/users/login', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify(userData),
-  });
+export const loginUser = async (userData) => {
+  try {
+    const { data } = await client.mutate({
+      mutation: LOGIN_USER,
+      variables: userData,
+    });
+    return data;
+  } catch (err) {
+    console.error(err);
+    throw new Error("Something went wrong");
+  }
 };
 
-// save book data for a logged in user
-export const saveBook = (bookData, token) => {
-  return fetch('/api/users', {
-    method: 'PUT',
-    headers: {
-      'Content-Type': 'application/json',
-      authorization: `Bearer ${token}`,
-    },
-    body: JSON.stringify(bookData),
-  });
+export const saveBook = async (bookData) => {
+  try {
+    const { data } = await client.mutate({
+      mutation: SAVE_BOOK,
+      variables: { bookData },
+    });
+    return data;
+  } catch (err) {
+    console.error(err);
+    throw new Error("Something went wrong");
+  }
 };
 
-// remove saved book data for a logged in user
-export const deleteBook = (bookId, token) => {
-  return fetch(`/api/users/books/${bookId}`, {
-    method: 'DELETE',
-    headers: {
-      authorization: `Bearer ${token}`,
-    },
-  });
+export const deleteBook = async (bookId) => {
+  try {
+    const { data } = await client.mutate({
+      mutation: REMOVE_BOOK,
+      variables: { bookId },
+    });
+    return data;
+  } catch (err) {
+    console.error(err);
+    throw new Error("Something went wrong");
+  }
 };
 
 // make a search to google books api
