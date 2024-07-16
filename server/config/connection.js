c437f5fac612fc149db90430a6222020606221e0
Soul <joshgingold336@gmail.com>
2024-07-16 13:26:04 -0700

    restarting

--- server/config/connection.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-require("dotenv").config();
-
-mongoose.connect(
-  process.env.MONGODB_URI || "mongodb://localhost:27017/googlebooks",
-  {}
-);
-
-module.exports = mongoose.connection;
